8de2638d09ae13344e2fce6f88aab33a47ded3e2
iamfaisalpk <iamfaizy123@gmail.com>
2025-07-23 13:20:48 +0530

    first commit

--- Server.js
+++ Server.js
@@ -46,21 +46,7 @@ app.get("/api/test", (req, res) => {
   });
 });
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); 
-  }
-
-  next();
-});
 
 // Routes
 app.use("/api/auth", authRoutes);
